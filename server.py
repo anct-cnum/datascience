3911cd57db9a49694eb19b8a7469cd7e6163573d
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-03-02 18:29:22 +0100

    test

--- server.py
+++ server.py
@@ -10,14 +10,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 app = Flask(__name__)
 
 @app.route('/')
-def app(environ, start_response):
-    data = b"Hello, World!\n"
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
-    ])
-
-    return iter([data])
+def app():
+    return 'Hello, World!'
 
 
 def jobs_create_modele():
