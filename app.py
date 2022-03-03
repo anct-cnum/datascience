25217ac3b79f644143567b8e707e4c71213ab5a4
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-03-03 15:30:37 +0100

    delete element

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
-import pandas as pd
-from bson import ObjectId
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import MinMaxScaler
-from src.mongodb import connect_db_prod
-from src.data.retrieveData import create_dataframe_prod
 from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/test')
 def index():
     return 'Hello, World!'
 
