df9d52df67cc441170d2479db32ea989ba7dbfef
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-03-03 18:40:35 +0100

    sds

--- app.py
+++ app.py
@@ -20,29 +20,24 @@ class Config:
 def index():
     return 'Hello, World!'
 
+# interval examples
+@scheduler.task("interval", id="do_job_1", seconds=30, misfire_grace_time=900)
+def job1():
+    """Sample job 1."""
+    print("Job 1 executed")
 
-@scheduler.task('cron', id='do_job_1', minute='*/15')
-def jobs_create_modele():
-    db = connect_db_prod()
-    data_conseillers = create_dataframe_prod()
-    dataframe_conseiller = pd.DataFrame(data_conseillers)
-    df_without_nan = dataframe_conseiller.dropna()
-    df_train_modele = df_without_nan.drop(columns=['conseiller_id', 'nom', 'prenom', 'email'])
-    df_train_modele[df_train_modele.columns] = MinMaxScaler().fit_transform(df_train_modele[df_train_modele.columns])
-    clf = KMeans(n_clusters=3)
-    clf.fit(df_train_modele)
-    labels = clf.labels_
-    df_without_nan['cluster'] = labels
-    result = pd.concat([dataframe_conseiller, df_without_nan['cluster']], axis=1)
-    result.loc[result.freqMeanCra.isnull() & result.nbJourLastCra.notnull(), 'cluster'] = 3
-    result.loc[result.nbJourLastCra.isnull(), 'cluster'] = 4
-    result.loc[
-        result.meanCraBySemaine.isnull() & result.nbJourLastCra.notnull() & result.freqMeanCra.notnull(), 'cluster'] = 5
-    result = result.fillna(0)
-    result['cluster'] = result['cluster'].astype('Int64')
-    for index, conseiller in result.iterrows():
-        db.conseillersTestQuentin.update_one({'_id': ObjectId(conseiller['conseiller_id'])},
-                                             {'$set': {"groupeCRA": conseiller['cluster']}})
+
+# cron examples
+@scheduler.task("cron", id="do_job_2", minute="*")
+def job2():
+    """Sample job 2."""
+    print("Job 2 executed")
+
+
+@scheduler.task("cron", id="do_job_3", week="*", day_of_week="sun")
+def job3():
+    """Sample job 3."""
+    print("Job 3 executed")
 
 
 if __name__ == '__main__':
