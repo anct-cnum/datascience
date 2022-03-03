4409c4915a62d0ce951db20f2eb4e1bc6dbc922c
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-03-03 16:46:11 +0100

    test jobs

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
 from flask import Flask
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
+class Config:
+    """App configuration."""
 
-@app.route('/test')
-def index():
-    return 'Hello, World!'
+    JOBS = [
+        {
+            "id": "job1",
+            "func": "jobs:job1",
+            "args": (1, 2),
+            "trigger": "interval",
+            "seconds": 10,
+        }
+    ]
 
+    SCHEDULER_API_ENABLED = True
+
+#@app.route('/test')
+#def index():
+ #   return 'Hello, World!'
+
+def job1(var_one, var_two):
+    """Demo job function.
+    :param var_two:
+    :param var_two:
+    """
+    print(str(var_one) + " " + str(var_two))
 
 if __name__ == '__main__':
+    app = Flask(__name__)
+    app.config.from_object(Config())
+
+    scheduler = APScheduler()
+    # it is also possible to enable the API directly
+    # scheduler.api_enabled = True  # noqa: E800
+    scheduler.init_app(app)
+    scheduler.start()
+
     app.run()
