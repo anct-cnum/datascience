6b3fd9fa2988af519799ebca911a193634aa6b23
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-03-03 16:41:55 +0100

    tess

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 from flask import Flask
-from flask_apscheduler import APScheduler
-import pandas as pd
-from bson import ObjectId
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import MinMaxScaler
-from src.mongodb import connect_db_prod
-from src.data.retrieveData import create_dataframe_prod
 
 app = Flask(__name__)
 
