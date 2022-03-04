7a1784ce6322e4657a448bf2c21223fdc0555460
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-03-04 10:45:38 +0100

    update modele

--- create_modele.sh
+++
@@ -1,5 +0,0 @@
-cd ${APP_HOME}
-
-echo "Entrainement du modele: START\n"
-python3 modele.py
-echo "Entrainement du modele: END\n"
--- modele.py
+++ modele.py
@@ -5,25 +5,24 @@ from sklearn.preprocessing import MinMaxScaler
 from src.mongodb import connect_db_prod
 from src.data.retrieveData import create_dataframe_prod
 
-
-def jobs_create_modele():
-    db = connect_db_prod()
-    data_conseillers = create_dataframe_prod()
-    dataframe_conseiller = pd.DataFrame(data_conseillers)
-    df_without_nan = dataframe_conseiller.dropna()
-    df_train_modele = df_without_nan.drop(columns=['conseiller_id', 'nom', 'prenom', 'email'])
-    df_train_modele[df_train_modele.columns] = MinMaxScaler().fit_transform(df_train_modele[df_train_modele.columns])
-    clf = KMeans(n_clusters=3)
-    clf.fit(df_train_modele)
-    labels = clf.labels_
-    df_without_nan['cluster'] = labels
-    result = pd.concat([dataframe_conseiller, df_without_nan['cluster']], axis=1)
-    result.loc[result.freqMeanCra.isnull() & result.nbJourLastCra.notnull(), 'cluster'] = 3
-    result.loc[result.nbJourLastCra.isnull(), 'cluster'] = 4
-    result.loc[
-        result.meanCraBySemaine.isnull() & result.nbJourLastCra.notnull() & result.freqMeanCra.notnull(), 'cluster'] = 5
-    result = result.fillna(0)
-    result['cluster'] = result['cluster'].astype('Int64')
-    for index, conseiller in result.iterrows():
-        db.conseillersTestQuentin.update_one({'_id': ObjectId(conseiller['conseiller_id'])},
-                                             {'$set': {"groupeCRA": conseiller['cluster']}})
+print("start jobs")
+db = connect_db_prod()
+data_conseillers = create_dataframe_prod()
+dataframe_conseiller = pd.DataFrame(data_conseillers)
+df_without_nan = dataframe_conseiller.dropna()
+df_train_modele = df_without_nan.drop(columns=['conseiller_id', 'nom', 'prenom', 'email'])
+df_train_modele[df_train_modele.columns] = MinMaxScaler().fit_transform(df_train_modele[df_train_modele.columns])
+clf = KMeans(n_clusters=3)
+clf.fit(df_train_modele)
+labels = clf.labels_
+df_without_nan['cluster'] = labels
+result = pd.concat([dataframe_conseiller, df_without_nan['cluster']], axis=1)
+result.loc[result.freqMeanCra.isnull() & result.nbJourLastCra.notnull(), 'cluster'] = 3
+result.loc[result.nbJourLastCra.isnull(), 'cluster'] = 4
+result.loc[
+    result.meanCraBySemaine.isnull() & result.nbJourLastCra.notnull() & result.freqMeanCra.notnull(), 'cluster'] = 5
+result = result.fillna(0)
+result['cluster'] = result['cluster'].astype('Int64')
+for index, conseiller in result.iterrows():
+    db.conseillersTestQuentin.update_one({'_id': ObjectId(conseiller['conseiller_id'])},
+                                         {'$set': {"groupeCRA": conseiller['cluster']}})
