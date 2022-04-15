b0a556e6cca446a93c527090caf712689bfad1f6
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-04-15 14:44:28 +0100

    version fonctionnelle

--- jobsTrainModele.py
+++ jobsTrainModele.py
@@ -4,12 +4,22 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
 from src.mongodb import connect_db_prod
 from src.data.retrieveData import create_dataframe_prod
+from datetime import datetime
+
+def checkChangeCluster(newCluster, oldCluster):
+    if newCluster == 0 and oldCluster == 1:
+        return False
+    elif newCluster == 1 and oldCluster == 0:
+        return False
+    else:
+        return True
+
 
 db = connect_db_prod()
 data_conseillers = create_dataframe_prod()
 dataframe_conseiller = pd.DataFrame(data_conseillers)
 df_without_nan = dataframe_conseiller.dropna()
-df_train_modele = df_without_nan.drop(columns=['conseiller_id', 'nom', 'prenom', 'email'])
+df_train_modele = df_without_nan.drop(columns=['conseiller_id', 'nom', 'prenom', 'email', 'groupeCRA'])
 df_train_modele[df_train_modele.columns] = MinMaxScaler().fit_transform(df_train_modele[df_train_modele.columns])
 clf = KMeans(n_clusters=3)
 clf.fit(df_train_modele)
@@ -20,8 +30,30 @@ result.loc[result.freqMeanCra.isnull() & result.nbJourLastCra.notnull(), 'cluste
 result.loc[result.nbJourLastCra.isnull(), 'cluster'] = 4
 result.loc[
     result.meanCraBySemaine.isnull() & result.nbJourLastCra.notnull() & result.freqMeanCra.notnull(), 'cluster'] = 5
-result = result.fillna(0)
 result['cluster'] = result['cluster'].astype('Int64')
+datetime_today = datetime.now()
 for index, conseiller in result.iterrows():
-    db.conseillers.update_one({'_id': ObjectId(conseiller['conseiller_id'])},
-                                         {'$set': {"groupeCRA": conseiller['cluster']}})
+    if conseiller["groupeCRA"] is not None:
+        last_cluster = conseiller['groupeCRA'][-1]
+        if last_cluster["numero"] != conseiller['cluster'] and checkChangeCluster(conseiller['cluster'], last_cluster["numero"]):
+            db.conseillers.update_one(
+                {
+                    '_id': ObjectId(conseiller['conseiller_id'])},
+                {'$push': {
+                    "GroupeCRA2": {
+                        "numero": conseiller['cluster'],
+                        "dateDeChangement": datetime_today,
+                        "nbJourDansCluster": (datetime_today - last_cluster["dateDeChangement"]).days
+                    }
+                }})
+
+    else:
+        db.conseillers.update_one(
+            {
+                '_id': ObjectId(conseiller['conseiller_id'])},
+            {'$push': {
+                "GroupeCRA2": {
+                    "numero": conseiller['cluster'],
+                    "dateDeChangement": datetime_today,
+                }
+            }})
--- src/data/retrieveData.py
+++ src/data/retrieveData.py
@@ -71,6 +71,11 @@ def create_feature_cra_prod(conseiller, db, datetime_today):
             mean_cra_by_week = sum(count_nb_cra_by_week.values()) / datetime_today.isocalendar()[1]
 
     nb_day_create = datetime_today - conseiller["dateFinFormation"]
+    try:
+        groupeCRA = conseiller["GroupeCRA2"]
+    except KeyError:
+        groupeCRA = None
+
     return {
         "conseiller_id": conseiller["_id"],
         "nom": conseiller["nom"],
@@ -80,7 +85,8 @@ def create_feature_cra_prod(conseiller, db, datetime_today):
         "nbJourLastCra": nb_day_last_cra,
         "meanCraBySemaine": mean_cra_by_week,
         "freqMeanCra": statistics.mean(freq_between_cra) if len(freq_between_cra) > 0 else None,
-        "countCra": count_cras_conseiller
+        "countCra": count_cras_conseiller,
+        "groupeCRA": groupeCRA
     }
 
 
@@ -112,6 +118,7 @@ def create_dataframe_prod():
             {'dateFinFormation': {'$lt': datetime_today - relativedelta(months=1)}}
         ]
     })
+
     for conseiller in conseillers:
         data_conseiller.append(create_feature_cra_prod(conseiller, db, datetime_today))
     return data_conseiller
